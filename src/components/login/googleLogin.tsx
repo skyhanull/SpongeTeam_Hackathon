36c1afdea9e3ace0760acc998fc4bbe272c02fc1
AnYoonKyoung <anky0425@naver.com>
2024-07-23 03:45:46 +0900

    fix(main):구글로그인 라이브러리 안쓰고 구현

--- src/components/login/googleLogin.tsx
+++ src/components/login/googleLogin.tsx
@@ -1,34 +1,41 @@
-// useGoogleLogin.js
-import React from 'react';
-import { useCallback } from 'react';
-import { GoogleLogin } from '@react-oauth/google';
+import React, { useEffect } from 'react';
 
-const useGoogleLogin = (onSuccess, onError) => {
-  const handleSuccess = useCallback(
-    (response) => {
-      console.log('Login Success:', response);
-      if (onSuccess) {
-        onSuccess(response);
-      }
-    },
-    [onSuccess]
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+function GoogleLogin() {
+  useEffect(() => {
+    if (clientId && window.google) {
+      window.google.accounts.id.initialize({
+        client_id: clientId,
+        callback: handleCredentialResponse,
+      });
+    } else {
+      console.error(
+        'Google client ID is missing or Google API script is not loaded.'
+      );
+    }
+  }, [clientId]);
+
+  const handleCredentialResponse = (response: any) => {
+    console.log('Login Success:', response);
+    localStorage.setItem('login', 'true');
+    // Handle the credential response here
+    // if (onLoginSuccess) {
+    //   onLoginSuccess(response);
+    // }
+  };
+
+  const handleLoginClick = () => {
+    window.google.accounts.id.prompt();
+  };
+
+  return (
+    <div>
+      <button onClick={handleLoginClick} className="bg-white border-none">
+        로그인
+      </button>
+    </div>
   );
+}
 
-  const handleError = useCallback(
-    (error) => {
-      console.error('Login Failed:', error);
-      if (onError) {
-        onError(error);
-      }
-    },
-    [onError]
-  );
-
-  const GoogleLoginButton = () => (
-    <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
-  );
-
-  return GoogleLoginButton;
-};
-
-export default useGoogleLogin;
+export default GoogleLogin;
