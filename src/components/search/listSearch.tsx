4cb5ea7eb45370e3cb32eab71c2016b4a10507bb
AnYoonKyoung <anky0425@naver.com>
2024-07-23 03:47:36 +0900

    style(main):css 변경

--- src/components/search/listSearch.tsx
+++ src/components/search/listSearch.tsx
@@ -5,7 +5,7 @@ const ListSearch = ({ state }: any) => {
   return (
     <div className="flex items-center justify-center flex-grow my-3">
       <input
-        className={`border-gray-200 border-2 rounded-full w-full px-3 ${
+        className={`border-gray-200 border-2 rounded-full w-full px-3 py-2 ${
           state ? 'bg-slate-100' : 'bg-white'
         }`}
         placeholder="검색어 입력"
